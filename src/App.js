78fddd6ff4aeeec8be40969fb9277e1df0b26467
galaxy4276 <galaxyhi4276@gmail.com>
2020-08-07 19:51:11 +0900

    Life Cycle Functions

--- src/App.js
+++ src/App.js
@@ -1,61 +1,52 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 
-const foodILike = [
-  {
-    id: 1,
-    name: 'Mango',
-    image: 'https://images.unsplash.com/photo-1546173159-315724a31696?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
-    rating: 5,
-  },
-  {
-    id: 2,
-    name: 'Pasta',
-    image: 'https://images.unsplash.com/photo-1563379926898-05f4575a45d8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
-    rating: 3.3,
-  },
-  {
-    id: 3,
-    name: 'Korean BBQ',
-    image: 'https://images.unsplash.com/photo-1527578054032-8d8f044e013d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80',
-    rating: 4.9,
-  },
-  {
-    id: 4,
-    name: 'Tiramisu',
-    image: 'https://images.unsplash.com/photo-1568627175565-25154711ef22?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
-    rating: 4.5,
-  },
-];
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log('hello');
+  }
 
+  componentDidMount() {
+    console.log('component rendered!');
+  }
 
-function Food({ name, picture, rating }) {
-  return (
-  <div>
-    <h2>I like { name }</h2>
-    <h3>rating: { rating } / 5.0 </h3>
-    <img src={ picture } alt={ name } />
-  </div>
-  );
-}
+  componentDidUpdate() {
+    console.log('I just updated.');
+  }
 
+  componentWillUnmount() {
+    console.log('Goodbye, cruel World.');
+  }
 
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired,
-}
+  state = {
+    count: 0,
+  };
 
+  add = () => {
+    this.setState(current => ({
+      count: this.state.count + 1,
+    }));
+  };
 
-function App() {
-  return (
-    <div>
-      { foodILike.map(dish => (<Food key={ dish.id } name={ dish.name } 
-      picture={ dish.image } rating={ dish.rating } />)) }
-    </div>
+  minus = () => {
+    this.setState(current => ({
+      count: this.state.count - 1,
+    }));
+  };
+
+  render() {
+    console.log(`I'm rendering.`);
+    return (
+      <div>
+        <h1>The number is: { this.state.count }</h1>
+        <button onClick={ this.add }>Add</button>
+        <button onClick={ this.minus }>Minus</button>
+      </div>
     );
+  }
 }
+
 
 
 export default App;
